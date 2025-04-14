42266aa795aaafb83c9e6455db624c154cb8a964
FahadAlHossain <fahadalhossain5@gmail.com>
2025-04-15 01:50:47 +0600

    sign in design done

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="font-bold my-10 text-2xl">Welcome to ReEstate</Text>
+      <Text className="font-bold my-10 text-2xl font-rubik">Welcome to ReEstate</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
